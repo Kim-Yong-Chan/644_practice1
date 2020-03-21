bde6e87a9bdf5cac4e9014c8aa0a5777e8369caf
Yongchan Kim <yongchankimkim@gmail.com>
2020-03-21 21:28:16 +0900

    Initial commit

---
+++ src/main/java/com/soongsil/sixfourfour/controller/UserController.java
@@ -0,0 +1,29 @@
+package com.soongsil.sixfourfour.controller;
+
+import com.soongsil.sixfourfour.model.User;
+import com.soongsil.sixfourfour.model.request.UserCreateRequest;
+import com.soongsil.sixfourfour.service.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+public class UserController {
+
+    @Autowired
+    private UserService userService;
+
+
+//    @GetMapping("/user/{id}")
+//    public void readUser(@PathVariable long id){
+//        userService.readUser(id);
+//    }
+
+    @RequestMapping(value = "/user/create", method = RequestMethod.POST)
+    public User createUser(@RequestBody UserCreateRequest userCreateRequest){
+
+        User user = userService.createUser(userCreateRequest.getName(), userCreateRequest.getPhone());
+        return user;
+
+    }
+
+}
